796a0a5a1596dfcff2edd293490b38c9a435c2db
grashuros <grashuros@icloud.com>
2026-04-05 17:11:23 +0300

    Add files via upload

---
+++ LR_TwentyOne/PresentationTier/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using LogicTier;
+
+namespace PresentationTier
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // Укажите путь к вашему файлу данных
+            string path = "teachers.txt"; 
+            DataContext = new UniversityManager(path);
+            InitializeComponent();
+        }
+    }
+}
